bc2c11b94c343d9d5b2d4bdfc1ea67ac7244e6cd
MaratGin <40231423+MaratGin@users.noreply.github.com>
2022-04-08 23:53:20 +0300

    initial commit

--- AnimationHW/ViewController.swift
+++ AnimationHW/ViewController.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet animationView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        UIView.animate(withDuration: 2.0) {
+            self.animationView.
+        } completion: { isFinish in
+            print("Finished")
+        }
     }
 
 
